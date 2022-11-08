adea02833bc4cf778add398820f24d3b9476e01a
orutara <orutaracom@gmail.com>
2022-11-08 21:01:04 +0900

    update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 const init = () => {
   const userProperties = PropertiesService.getUserProperties()
   userProperties.setProperties({
-    'spreadSheetId': 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-  });
+    spreadSheetId: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+  })
+}
+
+const settingSpreadSheet = () => {
+  const ss = SpreadsheetApp.getActiveSpreadsheet()
+  const sheet = ss.getSheetByName('SheetName')
+  const lastRow = sheet.getLastRow()
+  const range = sheet.getRange('A1:B1').setValues([xxx])
 }
